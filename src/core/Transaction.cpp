14387c3e333ec853633124c233d7b248dbc056d6
Vyacheslav O. Koshcheyev <vok1980@gmail.com>
2013-10-31 15:51:54 +0700

    remove modification timestamp
    No modifications for trunsactions expected

--- src/core/Transaction.cpp
+++ src/core/Transaction.cpp
@@ -21,10 +21,9 @@ int Transaction::GetId(t_id &refId)
 int Transaction::Serialize(ISerializer &serializer)
 {
 	int32_t iVersion = LAST_VERSION;
-
+ 
 	serializer.Serialize(iVersion);
 	serializer.Serialize(m_tsEvent);
-	serializer.Serialize(m_tsModification);
 	serializer.Serialize(m_amount);
 	serializer.Serialize(m_strName);
 	serializer.Serialize(m_strComment);
