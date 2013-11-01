014c6e9c24cef21707ceceda380d1134fd75ca9b
Vyacheslav O. Koshcheyev <vok1980@gmail.com>
2013-11-01 17:18:36 +0700

    adds XmlOutputter for unittests

--- src/unittests/main.cpp
+++ src/unittests/main.cpp
@@ -1,16 +1,29 @@
 
-
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+
+#include <cppunit/XmlOutputter.h>
+
 
 
 int main()
 {
     CppUnit::TextUi::TestRunner runner;
+    CppUnit::TestResultCollector collectedresults;
+    runner.eventManager().addListener(&collectedresults);
+    
     CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
     runner.addTest( registry.makeTest() );
-
-    bool wasSuccessful = runner.run( "", false );
-    return wasSuccessful ? 0 : 1;
     
+    runner.run( "", false );
+
+    std::ofstream xmlFileOut("UnittestResults.xml");
+    CppUnit::XmlOutputter xmlOut(&collectedresults, xmlFileOut);
+    xmlOut.write();
+    
+    return collectedresults.wasSuccessful() ? 0 : 1;
 }
+
