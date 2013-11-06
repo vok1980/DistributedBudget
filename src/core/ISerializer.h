28e0832ea770ea5bff847500bc2f092870829ebc
Vyacheslav O. Koshcheyev <vok1980@gmail.com>
2013-11-06 18:41:07 +0700

    add method to serialisation interface

--- src/core/ISerializer.h
+++ src/core/ISerializer.h
@@ -13,6 +13,7 @@ public:
 	virtual void Serialize(int32_t&) = 0;
 	virtual void Serialize(t_timestamp&) = 0;
 	virtual void Serialize(std::wstring&) = 0;
+    virtual void Serialize(std::string&) = 0;
 	virtual void Serialize(std::set<std::wstring>&) = 0;
 	virtual void Serialize(t_money&) = 0;
 };
