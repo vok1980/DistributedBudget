e99ec1e91e06cef9de5609988fea2c599993d9f0
Vyacheslav O. Koshcheyev <vok1980@gmail.com>
2013-11-06 17:10:59 +0700

    accuracy fix

--- src/core/Account.cpp
+++ src/core/Account.cpp
@@ -34,7 +34,7 @@ void Account::AddTransaction(t_Transaction_ptr pTransaction)
 t_money Account::StrikeBalance(void)
 {
 	t_Transaction_ptr pTransaction = m_pHeadTrunsuction;
-	t_money balance = 0;
+	t_money balance = 0.0;
 
 	while (NULL != pTransaction.get())
 	{
