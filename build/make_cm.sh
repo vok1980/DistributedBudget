6bd14fe84fa78aab77d6192b0de6c26660035081
vok1980 <vok1980@gmail.com>
2013-11-09 12:53:00 +0700

    create test target

--- build/make_cm.sh
+++ build/make_cm.sh
@@ -4,4 +4,4 @@ export PROJECT_BUILD_TYPE=Release
 export PROJECT_DIR="make"
 export PROJECT_GEN="Unix Makefiles"
 
-./cm.sh && cd project_make && make
+./cm.sh && cd project_make && make && make test
