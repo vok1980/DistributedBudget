96665ae0899800d8f83e0c98d8fe98a1318c4533
Vyacheslav Koshcheyev <vok1980@gmail.com>
2014-04-22 11:23:10 +0700

    implementing TDistributedItemsFactory

--- src/core/DistributedItemsFactory.h
+++ src/core/DistributedItemsFactory.h
@@ -1,36 +1,43 @@
 
 #pragma once
 
+//#include <list>
+#include <deque>
+
 #include "types.h"
 #include "TObjectHolder.h"
 #include "serializers/FileLoadSerializer.h"
 
 
-class DistributedItemsFactory
+template <typename TDistributedItem>
+class TDistributedItemsFactory
 {
 public:
-	template <typename TDistributedItem>
 	t_shared_ptr<TDistributedItem> CreateObject();
-
-	template <typename TDistributedItem>
 	t_shared_ptr<TDistributedItem> CreateObject(const t_DistibutedId&);
+
+private:
+	typedef std::deque<t_weak_ptr<TDistributedItem> > t_objects;
+	t_objects m_aCollection;
 };
 
 
 template <typename TDistributedItem>
-t_shared_ptr<TDistributedItem> DistributedItemsFactory::CreateObject()
+t_shared_ptr<TDistributedItem> TDistributedItemsFactory<TDistributedItem>::CreateObject()
 {
-	return t_shared_ptr<TDistributedItem>(new TDistributedItem());
+	t_shared_ptr<TDistributedItem> obj(new TDistributedItem());
+	m_aCollection.push_back(obj);
+	return obj;
 }
 
 
 template <typename TDistributedItem>
-t_shared_ptr<TDistributedItem> CreateObject(const t_DistibutedId &idItem)
+t_shared_ptr<TDistributedItem> TDistributedItemsFactory<TDistributedItem>::CreateObject(const t_DistibutedId &idItem)
 {
 	FileLoadSerializer loader("/temp");
 	TObjectHolder<TDistributedItem> holder(idItem);
 	holder.Serialize(loader);
-
+	m_aCollection.push_back(holder.GetObject());
 	return holder.GetObject();
 }
 
--- src/core/types.h
+++ src/core/types.h
@@ -4,9 +4,11 @@
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
 #include <tr1/memory>
 #define t_shared_ptr std::tr1::shared_ptr
+#define t_weak_ptr std::tr1::weak_ptr
 #else
 #include <memory>
 #define t_shared_ptr std::shared_ptr
+#define t_weak_ptr std::weak_ptr
 #endif	//__GXX_EXPERIMENTAL_CXX0X__
 
 #include <Poco/Timestamp.h>
